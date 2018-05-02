1446998c3fe461881dec984577e699197a7b1a7e
itggot-saga-losman <saga.losman@itggot.se>
2018-05-02 18:42:21 +0200

    CP 5 - BIFW "slideshow"

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 
 $('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
 
+
 $(document).ready(function(){
+    $(".drop").hide();
     $(".icon").click(function(){
         $(".drop").slideToggle("slow");
     });
